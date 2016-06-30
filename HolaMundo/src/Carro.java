c5681a17377e474360208fc384380bdd5471916f
ahernandez <ahernandez@telepro.com.mx>
2016-06-30 13:31:55 -0500

    Probando

--- HolaMundo/src/Carro.java
+++ HolaMundo/src/Carro.java
@@ -1,4 +1,36 @@
 
 public class Carro {
 
+	public String dog = "";
+	public int edad = 0;
+
+	/**
+	 * @return the dog
+	 */
+	public String getDog() {
+		return dog;
+	}
+
+	/**
+	 * @param dog the dog to set
+	 */
+	public void setDog(String dog) {
+		this.dog = dog;
+	}
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
