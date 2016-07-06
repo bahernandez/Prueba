994b0ed7c3415ed98dfe3af455ff69552d1b4396
ahernandez <ahernandez@telepro.com.mx>
2016-07-06 13:47:50 -0500

    Cambiando

--- HolaMundo/src/Carro.java
+++ HolaMundo/src/Carro.java
@@ -1,11 +1,11 @@
 
-
 public class Carro {
 
 	public String dog = "";
 	public int edad = 0;
 	public String llantas = "Michellin";
 	public int precio = 0;
+	private Double codigo = 0.0;
 
 	/**
 	 * @return the dog
@@ -61,6 +61,20 @@ public class Carro {
 	 */
 	public void setPrecio(int precio) {
 		this.precio = precio;
+	}
+
+	/**
+	 * @return the codigo
+	 */
+	public Double getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(Double codigo) {
+		this.codigo = codigo;
 	}
 	
 	
